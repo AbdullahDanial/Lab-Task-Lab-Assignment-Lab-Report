73776aa69b7eefb40d4af889aede89e02c9325f7
Fahim Abdullah Danial <105501133+Danial864@users.noreply.github.com>
2022-06-02 14:37:45 +0600

    Add files via upload

---
+++ Lab-Assignment/L-A-4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,n=1;
+    printf("enter the number of rows : ");
+    scanf("%d",&i);
+    for(a=1;a<=i;a++)
+    {
+        for(b=1;b<=a;++b)
+    
+        {printf("%d\t",n);
+        ++n;
+        
+        
+        
+        }
+        printf("\n");
+        
+        
+
+       
+     
+
+    }
+}
