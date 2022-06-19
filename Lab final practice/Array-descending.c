6ec8c1a649aeb775b5392c19e48f03e98c25f00b
Fahim Abdullah Danial <105501133+Danial864@users.noreply.github.com>
2022-06-19 16:57:21 +0600

    Add files via upload

---
+++ Lab final practice/Array-descending.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n[10],i,j,temp;
+    printf("Enter elements : \n");
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&n[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+        for(j=i+1;j<10;j++)
+        {
+            if(n[j]>n[i])
+            {
+                temp=n[i];
+                n[i]=n[j];
+                n[j]=temp;
+            }
+        }
+    }
+    printf("Array(Descending) =");
+    for(i=0;i<10;i++)
+    {
+        printf("%d\t",n[i]);
+    }
+
+}
